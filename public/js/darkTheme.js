eb66a448ff1ac54b95d2829f1b7430b7f44dcbb3
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2023-07-13 15:34:49 -0300

    teste

--- public/js/darkTheme.js
+++ public/js/darkTheme.js
@@ -1,11 +1,11 @@
 /* ALTERANDO PARA O MODO DARK-----------------------------------------*/
 
-export function darkTheme(){
-    const dark = document.getElementById('botao-1');
+export default function darkTheme(){
+    // const dark = document.getElementById('botao-1');
 
-    function modoDark(){
-        document.body.classList.toggle('tema-escuro');  
-    }
+    // function modoDark(){
+    //     document.body.classList.toggle('tema-escuro');  
+    // }
 
-    dark.addEventListener('click', modoDark);
+    // dark.addEventListener('click', modoDark);
 }
--- public/js/modal.js
+++ public/js/modal.js
@@ -1,25 +1,25 @@
-export function modal(){
-    const modalOverlay = document.querySelector(".modal_overlay");
-    const cards = document.querySelectorAll(".card");
+export default function modal(){
+    // const modalOverlay = document.querySelector(".modal_overlay");
+    // const cards = document.querySelectorAll(".card");
     
-    for (let card of cards){
-        card.addEventListener("click", function(){
-            const imagemID = card.getAttribute("id")
-            const textCard = card.querySelector('.card_titulo').innerHTML 
-            /*no textCard ele irá pegar todo o conteúdo que estará em card_titulo e colocar na constante textCard*/
+    // for (let card of cards){
+    //     card.addEventListener("click", function(){
+    //         const imagemID = card.getAttribute("id")
+    //         const textCard = card.querySelector('.card_titulo').innerHTML 
+    //         /*no textCard ele irá pegar todo o conteúdo que estará em card_titulo e colocar na constante textCard*/
 
-            modalOverlay.classList.add("active");
-            modalOverlay.querySelector(".modal_conteudo img").src = `${imagemID}`
-            modalOverlay.querySelector('.titulosModal').innerHTML  = textCard
-            /*em ModalOverlayselecionando o objeto titulosModal é repassado as informações contidas em textCard assim mostrando as informações que estão no card no MODAL*/
+    //         modalOverlay.classList.add("active");
+    //         modalOverlay.querySelector(".modal_conteudo img").src = `${imagemID}`
+    //         modalOverlay.querySelector('.titulosModal').innerHTML  = textCard
+    //         /*em ModalOverlayselecionando o objeto titulosModal é repassado as informações contidas em textCard assim mostrando as informações que estão no card no MODAL*/
 
-            /*Para essa questão do modal você pode pesquisar sobre innerHTML ou textContent*/
+    //         /*Para essa questão do modal você pode pesquisar sobre innerHTML ou textContent*/
 
-        })
-    }
+    //     })
+    // }
 
-    document.querySelector(".close_modal").addEventListener("click", function(){
-        modalOverlay.classList.remove("active");
-        modalOverlay.querySelector("modal_conteudo img").src = ""
-    });
+    // document.querySelector(".close_modal").addEventListener("click", function(){
+    //     modalOverlay.classList.remove("active");
+    //     modalOverlay.querySelector("modal_conteudo img").src = ""
+    // });
 }
