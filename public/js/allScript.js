8997aaec68abf5e164741010c959c2784eb462c4
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2023-07-13 20:51:14 -0300

    Cards dinâmicos

--- public/js/allScript.js
+++ public/js/allScript.js
@@ -1,10 +1,12 @@
 import buttonMenu from './buttonMenu.js';
 import modal from './modal.js';
-import darkTheme from './darkTheme.js'
+import darkTheme from './darkTheme.js';
+import cards from './cards.js';
 
 buttonMenu();
 modal();
 darkTheme();
+cards();
 
 
 
