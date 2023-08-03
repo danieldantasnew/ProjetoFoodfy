59de81279827969788be0982fa8ce1e85fe587f0
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2023-08-03 18:50:11 -0300

    algumas correções de org de codigo

--- public/js/pageRedirect.js
+++ public/js/pageRedirect.js
@@ -1,15 +1,16 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-use-before-define */
 import observadorAction from './observador.js';
 
 export default class RedirecionaPagina {
-  constructor(cardsFather){
+  constructor(cardsFather) {
     this.cardsFather = document.querySelector(cardsFather);
 
     this.hearClick = this.hearClick.bind(this);
   }
 
-  redirecione(index) {
-    console.log('oi')
+  redirecione(index, eventClick) {
+    eventClick.preventDefault();
     localStorage.index = index;
     window.location.href = 'receitaUnica.html';
   }
@@ -17,18 +18,16 @@ export default class RedirecionaPagina {
   hearClick() {
     const cards = document.querySelectorAll('.card');
     const events = ['click', 'touchstart'];
-    console.log()
 
     cards.forEach((card, index) => {
       events.forEach((evento) => {
-        card.addEventListener(evento, () => {
-          this.redirecione(index);
-        })
+        card.addEventListener(evento, (eventClick) => {
+          this.redirecione(index, eventClick);
+        });
       });
     });
     this.desconectar.disconnect();
   }
-
 
   init() {
     if (this.cardsFather) {
@@ -36,4 +35,3 @@ export default class RedirecionaPagina {
     }
   }
 }
-
